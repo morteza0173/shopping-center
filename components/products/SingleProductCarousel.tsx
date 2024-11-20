c6638ea50a0a83f5997414b5908e48b51ae715cc
morteza0173 <morteza0173@outlook.com>
2024-11-20 12:01:17 +0330

    Thumbnail Carousel change

--- components/products/SingleProductCarousel.tsx
+++ components/products/SingleProductCarousel.tsx
@@ -1,90 +1,3 @@
-// "use client";
-// import { Product } from "@/utils/type";
-// import { Card, CardContent } from "@/components/ui/card";
-// import { type CarouselApi } from "@/components/ui/carousel";
-
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel";
-// import Image from "next/image";
-// import React from "react";
-
-// function SingleProductCarousel({ product }: { product: Product }) {
-//   const [api, setApi] = React.useState<CarouselApi>();
-//   const [current, setCurrent] = React.useState(0);
-//   const [count, setCount] = React.useState(0);
-
-//   React.useEffect(() => {
-//     if (!api) {
-//       return;
-//     }
-
-//     setCount(api.scrollSnapList().length);
-//     setCurrent(api.selectedScrollSnap() + 1);
-
-//     api.on("select", () => {
-//       setCurrent(api.selectedScrollSnap() + 1);
-//     });
-//   }, [api]);
-
-//   const { images, name } = product;
-//   return (
-//     <>
-//       <div className="w-full flex justify-center items-center h-72">
-//         <Carousel className="w-10/12 h-72" setApi={setApi}>
-//           <CarouselContent>
-//             {images.map((image, index) => {
-//               return (
-//                 <CarouselItem key={index}>
-//                   <Card>
-//                     <CardContent className="w-full h-full flex items-center justify-center">
-//                       <Image
-//                         src={image.src}
-//                         alt={name}
-//                         width={150}
-//                         height={150}
-//                         className="w-full h-full object-cover mt-4 rounded"
-//                       />
-//                     </CardContent>
-//                   </Card>
-//                 </CarouselItem>
-//               );
-//             })}
-//           </CarouselContent>
-//         </Carousel>
-//       </div>
-//       <div className="flex justify-center w-full mt-32">
-//         <Carousel className="w-10/12" setApi={setApi}>
-//           <CarouselContent>
-//             {images.map((image, index) => {
-//               return (
-//                 <CarouselItem key={index} className="basis-1/3">
-//                   <Card>
-//                     <CardContent className="flex items-center justify-center">
-//                       <img
-//                         src={image.src}
-//                         alt={name}
-//                         className="mt-4 object-cover rounded"
-//                       />
-//                     </CardContent>
-//                   </Card>
-//                 </CarouselItem>
-//               );
-//             })}
-//           </CarouselContent>
-//           <CarouselPrevious />
-//           <CarouselNext />
-//         </Carousel>
-//       </div>
-//     </>
-//   );
-// }
-// export default SingleProductCarousel;
-
 "use client";
 import { Product } from "@/utils/type";
 import { Card, CardContent } from "@/components/ui/card";
@@ -173,19 +86,19 @@ function SingleProductCarousel({ product }: { product: Product }) {
               return (
                 <CarouselItem
                   key={index}
-                  className={`basis-1/3 flex justify-center item`}
+                  className={`basis-1/3 cursor-pointer`}
                   onClick={() => handleThumbnailClick(index)}
                 >
-                  <Card>
+                  <Card className="w-full h-full flex items-center justify-center">
                     <CardContent
-                      className={`flex items-center justify-center  ${
+                      className={`flex items-center justify-center  p-1 w-full h-full ${
                         currentIndex === index ? "border border-blue-500" : ""
                       } `}
                     >
                       <img
                         src={image.src}
                         alt={name}
-                        className="mt-4 object-cover rounded"
+                        className=" object-cover rounded"
                       />
                     </CardContent>
                   </Card>
